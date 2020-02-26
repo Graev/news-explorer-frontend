01391bfe48f84f21205089d9f23a3323edbef6f3
Graev <54083933+Graev@users.noreply.github.com>
2020-02-27 02:58:30 +0300

    add project

---
+++ webpack.config.js
@@ -0,0 +1,126 @@
+// webpack.config.js
+const webpack = require("webpack");
+// new webpack.DefinePlugin({
+//     'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+// })
+
+const path = require("path");
+// Подключили к проекту плагин
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+// Плагин для hot reload
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+// плагин для хеширования
+const WebpackMd5Hash = require("webpack-md5-hash");
+// подключаем path к конфигу вебпак
+const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+
+const ghpages = require("gh-pages");
+const fs = require("fs");
+
+const isDev = process.env.NODE_ENV === "development";
+const pages = fs
+  .readdirSync(path.resolve(__dirname, "src"))
+  .filter(fileName => fileName.endsWith(".html"));
+// создаем переменную для development-сборки
+module.exports = {
+  entry: { main: "./src/script/index.js", lk: "./src/script/lk.js" },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].[chunkhash].js"
+  },
+  module: {
+    rules: [
+      {
+        // тут описываются правила
+        test: /\.js$/, // регулярное выражение, которое ищет все js файлы
+        use: { loader: "babel-loader" }, // весь JS обрабатывается пакетом babel-loader
+        exclude: /node_modules/ // исключает папку node_modules
+      },
+      {
+        test: /\.css$/,
+        use: [
+          isDev ? "style-loader" : MiniCssExtractPlugin.loader,
+          "css-loader",
+          "postcss-loader"
+        ],
+        exclude: /node_modules/ // исключает папку node_modules
+      },
+      {
+        test: /\.(eot|ttf|woff|woff2)$/,
+        loader: "file-loader?name=./vendor/[name].[ext]",
+        exclude: /node_modules/ // исключает папку node_modules
+      },
+      {
+        test: /\.(jpg|gif|ico|svg)$/,
+        use: [
+          "file-loader?name=../src/images/[name].[ext]", // указали папку, куда складывать изображения
+          {
+            loader: "image-webpack-loader",
+            options: {
+              mozjpeg: {
+                progressive: true,
+                quality: 65
+              },
+              // optipng.enabled: false will disable optipng
+              optipng: {
+                enabled: true
+              },
+              pngquant: {
+                quality: [0.65, 0.9],
+                speed: 4
+              },
+              gifsicle: {
+                interlaced: false
+              },
+              // the webp option will enable WEBP
+              webp: {
+                quality: 75
+              }
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        loader: "file-loader",
+        options: {
+          name(file) {
+            if (process.env.NODE_ENV === "development") {
+              return "[path][name].[ext]";
+            }
+
+            return "[contenthash].[ext]";
+          },
+          outputPath: "images"
+        }
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].[contenthash].css",
+      chunkFilename: "[name].[contenthash].css"
+    }),
+    new OptimizeCssAssetsPlugin({
+      assetNameRegExp: /\.css$/g,
+      cssProcessor: require("cssnano"),
+      cssProcessorPluginOptions: {
+        preset: ["default"]
+      },
+      canPrint: true
+    }), // подключите плагин после MiniCssExtractPlugin
+
+    new HtmlWebpackPlugin({
+      template: `./src/index.html`,
+      filename: "index.html"
+    }),
+    new HtmlWebpackPlugin({
+      template: `./src/lk.html`,
+      filename: "lk.html"
+    }),
+    new WebpackMd5Hash(),
+    new webpack.DefinePlugin({
+      NODE_ENV: JSON.stringify(process.env.NODE_ENV)
+    })
+  ]
+};
